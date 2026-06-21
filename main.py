084241d73408cb28c5047790d84175e36b2dad52
SAUDAYA SATAPATHY <saudayasatapathy@Mac.lan>
2026-06-21 10:12:24 +0530

    Add add_task with input validation and type annotations

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-def add_task(name: str,priority: int) -> None:
-    if name == "":
+def add_task(name: str,priority: int) -> dict:
+    if name.strip() == "":
         print("Task name cannot be empty.")
-        return
+        return {}
+    if priority > 7:
+        print("Priority cannot be greater or equal to 7.")
+        return {}
     if priority < 0:
         print("Priority cannot be negative.")
-        return 
-    print(f"Task added: {name} with priority {priority}")
+        return {}
+    return {"name": name,"priority": priority}
 
+add_task("Arise Beru", 1)
 
-
-
-
-
-
-
-
-
-
-
-add_task("Buy groceries", 2)
-add_task("", 2)
-add_task("Buy groceries", -1)
+add_task("Arise Beru", -2)
